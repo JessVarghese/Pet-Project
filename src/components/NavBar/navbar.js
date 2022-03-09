2f09a1c1a1c4ce22f7a71ccb66afb82482fde038
Jess Varghese <jessvarghese@gmail.com>
2022-03-09 14:55:26 -0500

    adding MUI for styling and page header

---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import PetLogo from '../../images/pet-house_blk_wt.png'
+import { Avatar } from '@mui/material';
+import { palette } from '@mui/system';
+
+
+
+
+function TopNav() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{bgcolor: 'success.light'}} position="static" >
+         <Avatar alt='petLogo' src={PetLogo} sx={{ mx:'auto', width: 200, height: 200 }} variant="square" />
+        <Toolbar>
+          
+          <Typography variant="h3" component="div" sx={{
+          mx: 'auto', p:'auto'}}>
+            The Pet Project
+          </Typography>
+         
+        </Toolbar>
+      </AppBar>
+    </Box>
+    
+  );
+}
+
+
+export default TopNav;
